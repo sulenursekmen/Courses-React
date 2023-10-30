e589fba57bdfee2e95e715186f31e960248f8471
sulenursekmen <sulenursekmenn@gmail.com>
2023-10-30 16:24:36 +0300

    Courses cards

---
+++ src/Courses.js
@@ -0,0 +1,23 @@
+
+import Course from './Course';
+function Courses({courses,removeCourse}) {
+    console.log(courses);
+    return ( <div className='courseMainDiv'>
+<div>
+    <h2>Kurslarım</h2>
+</div>
+
+<div className="cardDiv">
+ {
+    courses.map((course)=>{
+        return (
+          <Course key={course.id} {...course} removeOneCourse={removeCourse}/>
+        //   course={course}
+        );
+    })
+ }
+</div>
+    </div> );
+}
+
+export default Courses;
